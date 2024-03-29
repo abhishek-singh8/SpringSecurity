5330f50841c99d24410c608f6e3183dc9cbe557c
abhishek kumar singh <abhishek.k.singh@boeing.com>
2024-03-29 13:20:38 +0530

    basic config along with customer SecurityFilterChain

---
+++ springsecuritysection2/src/main/java/com/learn/springsecuritysection2/BankApplicationBackend.java
@@ -0,0 +1,13 @@
+package com.learn.springsecuritysection2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplicationBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplicationBackend.class, args);
+	}
+
+}
